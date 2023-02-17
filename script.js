7afaf36b2e9d2ead8478caf1915cf3e03c10a9c3
asacchet <64910798+asacchet@users.noreply.github.com>
2023-02-16 22:05:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const element = document.getElementById("video");
+const element_2 = document.getElementById("music");
+
+
+function scrollToBottom() {
+  element.scrollIntoView(false);
+}
+
+function scrollToTop() {
+  element_2.scrollIntoView(false);
+}
